9f22c35ec79dda93a41ebd1684dbe828580620de
Albina539 <albina.dj2405@gmail.com>
2026-06-22 23:02:47 +0300

    create main page, add style and add mobile style

--- slider/src/App.tsx
+++ slider/src/App.tsx
@@ -1,10 +1,42 @@
 import "./App.css";
+import starsBg from "../src/assets/stars-bg2.svg";
+import cloudBg from "../src/assets/cloud-down.svg";
 import Hero from "./components/custom/Hero";
+import { ArrowBigUp } from "pixelarticons/react";
+import GideSection from "./components/custom/GideSection";
 
 function App() {
   return (
-    <div className="bg-black">
+    <div className="h-full min-h-screen w-full bg-black">
       <Hero></Hero>
+      <GideSection />
+      <div className="md:mb-10 mb-5 flex justify-center">
+        <img src={starsBg} alt="stars-bg" width={1100} />
+      </div>
+      <div className="flex justify-center items-centers flex-col relative">
+        <div className="absolute inset-0 w-full">
+          <img
+            src={cloudBg}
+            alt="Cloud background"
+            className="w-full md:h-full h-95 object-cover overflow-visible"
+          />
+        </div>
+        <div className="flex items-center justify-center pt-50 relative z-10 md:h-120 h-90">
+          <a href="#">
+            <div className="flex gap-10 justify-center items-center">
+              <ArrowBigUp
+                style={{ width: "96px", height: "96px" }}
+                className="text-white"
+              />
+              <h2 className="text-white md:text-7xl text-4xl">Вверх</h2>
+              <ArrowBigUp
+                style={{ width: "96px", height: "96px" }}
+                className="text-white"
+              />
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ slider/src/components/custom/GideSchema.tsx
@@ -0,0 +1,77 @@
+import mascot from "../../assets/logo.svg";
+import arrow from "../../assets/arrow-right.svg";
+import arrowDown from "../../assets/arrow-down.svg";
+
+const GideSchema = () => {
+  return (
+    <div className="flex flex-col overflow-hidden md:mb-25 mb-15 items-center">
+      <div className="relative flex max-md:flex-col justify-center items-center md:mb-12 mb-5 md:right-50">
+        <div className="flex flex-col items-start max-md:mb-5">
+          <img src={mascot} alt="mascot" className="md:w-24 w-16" />
+          <span className="flex items-center justify-center text-black bg-[#2eff3c] px-6 py-3 md:text-3xl text-xl md:w-100 w-70 md:h-24 h-18 text-center">
+            Войди в аккаунт
+          </span>
+        </div>
+        <img
+          src={arrow}
+          alt="arrow"
+          className="md:block hidden w-32 h-32 object-contain absolute left-1/2 translate-x-55 top-25"
+        />
+        <img
+          src={arrowDown}
+          alt="arrow-down"
+          className="md:hidden block w-32 h-32"
+        />
+      </div>
+
+      <div className="relative flex max-md:flex-col justify-center items-center md:mb-12 mb-5 md:left-50">
+        <img
+          src={arrow}
+          alt="arrow"
+          className="md:block hidden w-32 h-32 object-contain absolute right-1/2 -translate-x-55 top-25 rotate-270"
+        />
+        <div className="flex flex-col items-end max-md:mb-5 max-md:-mt-10">
+          <img src={mascot} alt="mascot" className="md:w-24 w-16" />
+          <span className="flex items-center justify-center text-black bg-[#2eff3c] px-6 py-3 md:text-3xl text-xl md:w-100 w-70 md:h-24 h-18 text-center">
+            Загрузи свой текст
+          </span>
+        </div>
+        <img
+          src={arrowDown}
+          alt="arrow-down"
+          className="md:hidden block w-32 h-32"
+        />
+      </div>
+
+      <div className="relative flex max-md:flex-col justify-center items-center md:mb-12 mb-5 md:right-50">
+        <div className="flex flex-col items-start max-md:mb-5 max-md:-mt-10">
+          <img src={mascot} alt="mascot" className="md:w-24 w-16" />
+          <span className="flex items-center justify-center text-black bg-[#2eff3c] px-6 py-3 md:text-3xl text-xl md:w-100 w-70 md:h-24 h-18 text-center">
+            Выбери параметры презентации
+          </span>
+        </div>
+        <img
+          src={arrow}
+          alt="arrow"
+          className="md:block hidden w-32 h-32 object-contain absolute left-1/2 translate-x-55 top-25"
+        />
+        <img
+          src={arrowDown}
+          alt="arrow-down"
+          className="md:hidden block w-32 h-32"
+        />
+      </div>
+
+      <div className="relative flex max-md:flex-col justify-center items-center md:left-50">
+        <div className="flex flex-col items-end max-md:-mt-10">
+          <img src={mascot} alt="mascot" className="md:w-24 w-16" />
+          <span className="flex items-center justify-center text-black bg-[#2eff3c] px-6 py-3 md:text-3xl text-xl md:w-100 w-70 md:h-24 h-18 text-center">
+            Все готово!
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GideSchema;
